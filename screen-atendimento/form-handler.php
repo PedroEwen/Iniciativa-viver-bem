c116e55d881e9c573e175eeb362db3d0e9aad82c
Pedro Henrique <pedrohenriquewen@gmail.com>
2023-11-07 11:47:11 -0300

    Correções

--- screen-atendimento/form-handler.php
+++ screen-atendimento/form-handler.php
@@ -4,15 +4,15 @@ $visitor_email = $_POST['email'];
 $cpf = $_POST['cpf'];
 $message = $_POST['message'];
 
-$email_from = 'pedrohenriquewen@gmail.com'
-$email_suject = 'Nova solicitação de consulta'
+$email_from = 'pedrohenriquewen@gmail.com';
+$email_subject = 'Nova solicitação de consulta';
 $email_body = "Nome do paciente: $name.\n".
                 "Email do paciente: $visitor_email.\n".
                 "CPF do paciente: $cpf.\n".
                 "Mensagem: $message.\n";
 
-$to = 'pedrohenriquewen7@gmail.com'
-$headers = "From: $email_from" 
+$to = 'pedrohenriquewen7@gmail.com';
+$headers = "From: $email_from" ;
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_suject, $email_body, $headers);
