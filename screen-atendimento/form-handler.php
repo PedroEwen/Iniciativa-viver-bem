456359dd6df8e168822e1682b4db385e750a7182
PedroEwen <pedrohenriquewen@gmail.com>
2023-11-09 13:13:14 -0300

    Alterações e PHP

--- screen-atendimento/form-handler.php
+++ screen-atendimento/form-handler.php
@@ -1,20 +1,24 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$cpf = $_POST['cpf'];
-$message = $_POST['message'];
 
-$email_from = 'pedrohenriquewen@gmail.com';
-$email_subject = 'Nova solicitação de consulta';
-$email_body = "Nome do paciente: $name.\n".
-                "Email do paciente: $visitor_email.\n".
-                "CPF do paciente: $cpf.\n".
-                "Mensagem: $message.\n";
+if (isset($_POST['email']) && !empty$_POST['email']) {
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$cpf = addslashes($_POST['cpf']);
+$subject = addslashes($_POST['subject']);
+$message = addslashes($_POST['message']);
 
-$to = 'pedrohenriquewen7@gmail.com';
-$headers = "From: $email_from" ;
-$headers .= "Reply-To: $visitor_email \r\n";
+$to = "inciativa.viver.bem@gmail.com";
+$body = "Nome do paciente: ".$nome."\r\n".
+        "Email do paciente: ".$email."\r\n".
+        "Mensagem: ".$message;
+$header = "From:pzinhobr@gmail.com"."\r\n".
+            "Reply-To: $email"."\e\n".
+            "X=Mailer:PHP/".phpversion
 
-mail($to, $email_suject, $email_body, $headers);
-header("Location: atendimento.html");
+if (mail($to, $subject, $body, $header)) {
+    echo("Email enviado com sucesso");
+}else{
+    echo("Email não ser enviado");  
+}
+}
 ?>
